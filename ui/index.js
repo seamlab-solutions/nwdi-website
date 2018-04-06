d86e6491aa518f72168d19cf023829a072598b2f
seamlab-solutions <36943966+seamlab-solutions@users.noreply.github.com>
2018-04-06 16:50:28 +0800

    Clinics, OR Image

--- ui/index.js
+++ ui/index.js
@@ -15,80 +15,551 @@ function show_clinic(id = null) {
 			address: '#250 D. Tuazon Street, Quezon City', 
 			telephone: '(02) 790 8888',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics',
+						'4D Ultrasound'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'BONE DENSITOMETRY',
+				],
+				[
+					'PULMONARY FUNCTION TEST',
+				],
+				[
+					'DUPLEX SCAN',
+					[
+						'Arterial',
+						'Carotid',
+						'Renal',
+						'Venous'
+					]
+				],
+				[
+					'STRESS TEST',
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'G/F Philippines College of Surgeons Bldg. 992 EDSA, Quezon City', 
 			telephone: '(02)426 7887',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Unit 5 Olympia Commercial Plaza, 131 Quirino Highway Baesa, Quezon City', 
 			telephone: '(02)330 0258 ',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: '188 General Luis St. Brgy. Nagkaisang-Nayon Novaliches, Quezon City ', 
 			telephone: '(02)790 8855',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Unit 12 Ponciana Center McArthur Highway cor. Del Monte Ave. Potrero, Malabon', 
 			telephone: '(02)330 9245 ',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Units 229-231 Dasmariñas St. Binondo, Manila', 
 			telephone: '(02)242 0641 ',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics',
+						'4D Ultrasound'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'BONE DENSITOMETRY',
+				],
+				[
+					'PULMONARY FUNCTION TEST',
+				],
+				[
+					'DUPLEX SCAN',
+					[
+						'Arterial',
+						'Carotid',
+						'Renal',
+						'Venous'
+					]
+				],
+				[
+					'STRESS TEST',
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Mirasol Bldg., Apacible St. cor. Taft Ave. Manila', 
 			telephone: '(02)521-0320 / 790-8860',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics'
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Rm. 104 Campos Rueda Bldg., 101 Urban Ave. San Lorenzo, Makati', 
 			telephone: '(02)887 5498 ',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics',
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Analog'
+					]
+				],
+				[
+					'DRUG TEST',
+				],
+				
+				[
+					'DUPLEX SCAN',
+					[
+						'Arterial',
+						'Venous'
+					]
+				],
+				[
+					'STRESS TEST',
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Meralco Building Ortigas, Metro Manila', 
 			telephone: '(02)242 0641 ',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General'
+						
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Digital'
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				[
+					'MAMOGRAPHY',
+					[
+						'Digital'
+					]
+				],
+				[
+					'PULMONARY FUNCTION TEST',
+				],
+				[
+					'DUPLEX SCAN',
+					[
+						'Arterial',
+						'Carotid',
+						'Renal',
+						'Venous'
+					]
+				],
+				[
+					'CT SCAN',
+				],
+				[
+					'STRESS TEST',
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'EXECUTIVE CHECK UP',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		},
 		{ 
 			address: 'Imperial Appliance Plaza Brgy. Talon Uno Alabang-Zapote Road Las Piñas', 
 			telephone: '(02)790 8890  ',
 			services: [
-				'Services 1',
-				'Services 2'
+				[
+					'COMPLETE LABORATORY TESTS'
+				],
+				[
+					'ULTRASOUND',
+					[
+						'General',
+						'Obstetrics',
+					]
+				],
+				[
+					'XRAY',
+					[
+						'Conventional Film '
+					]
+				],
+				[
+					'DIGITAL ECG',
+				],
+				[
+					'2D ECHO',
+					[
+						'Plain',
+						'With Doppler'
+					]
+				],
+				
+				[
+					'DRUG TEST',
+				],
+				[
+					'HOME SERVICE',
+				],
+				[
+					'WALK-IN PRE EMPLOYMENT EXAM (PEE)',
+				],
+				[
+					'WALK-IN ANNUAL PHYSICAL EXAM ( APE)',
+				],
+			
 			]
 		}
 	]
